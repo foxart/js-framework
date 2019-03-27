d999d069ccca73e6fa4e83a408bb6c29b0eae43c
ivan <ivan@foxart.org>
2019-03-27 11:19:30 +0200

    pre patch commit

--- ~publish.sh
+++ ~publish.sh
@@ -4,20 +4,25 @@ for git_cache in $(git ls-files -i --exclude-standard)
 do echo "$git_cache" && git rm -f --cached "$git_cache"
 done
 
-git add -A
-git commit -a -m "pre patch commit"
-npm version patch
-VERSION=`npm view fa-nodejs version`
-git commit -a -m "patch to $VERSION"
-git push
-git push --tags
-npm publish
+#VERSION=$(npm version patch)
+VERSION=`git describe --abbrev=0 --tags`
+VERSION_BITS=(${VERSION//./ })
+TAG1=${VERSION_BITS[0]}
+TAG2=${VERSION_BITS[1]}
+TAG3=${VERSION_BITS[2]}
+TAG3=$((TAG3+1))
+TAG="$TAG1.$TAG2.$TAG3"
+COMMIT=`git rev-parse HEAD`
+MESSAGE=`git log -1`
+PUBLISHED=`git describe --contains ${COMMIT}`
 
-#git tag "$VERSION" -a -m "$VERSION"
-#git tag "$VERSION"
-#git push origin "$VERSION"
-#git push --tags
-#git tag -d "v$VERSION"
-#git push --delete origin "v$VERSION"
-#bower version patch -m "patched to %s"
-#bower register fa-javascript git://github.com/foxart/fa-javascript.git
+if [[ -z "$PUBLISHED" ]]; then
+    echo "updating tag <$VERSION> to version <$TAG>"
+    git commit -a -m "${TAG}"
+    git push
+    git tag ${TAG}
+    git push --tags
+    npm publish
+else
+    echo "already have tag <$VERSION> on commit <$COMMIT>"
+fi
