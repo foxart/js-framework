8a0cbbb73353f88198379de70f9131506ccccfa2
ivan <ivan@foxart.org>
2019-04-19 17:43:06 +0300

    U1-24 #https://lantec.atlassian.net/browse/U1-24

--- server/http-status-code.js
+++ server/http-status-code.js
@@ -4,12 +4,14 @@ class FaServerHttpStatusCode {
 	constructor() {
 		//http
 		this.badRequest = 400;
-		this.internalServerError = 500;
+		this.forbidden = 403;
 		this.found = 302;
+		this.internalServerError = 500;
 		this.movedPermanently = 301;
 		this.notFound = 404;
 		this.notImplemented = 501;
 		this.ok = 200;
+		this.unauthorized = 401;
 		//api
 		this.apiGet = 200;
 		this.apiDelete = 204;
