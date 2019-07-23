f3e5a170135c7885511cc4e52964e91ae0cae3c4
ivan <ivan@foxart.org>
2019-07-23 15:50:37 +0300

    fix

--- publish.sh
+++ publish.sh
@@ -1,27 +1,9 @@
 #!/usr/bin/env bash
-## GULP
-#npm install --save-dev gulp
-#npm install --save-dev gulp-babel
-#npm install --save-dev gulp-browserify
-#npm install --save-dev gulp-changed
-#npm install --save-dev gulp-debug
-#npm install --save-dev gulp-exec
-#npm install --save-dev gulp-less
-#npm install --save-dev gulp-notify
-#npm install --save-dev gulp-plumber
-#npm install --save-dev gulp-sourcemaps
-## BABEL
-#npm install --save-dev @babel/core
-#npm install --save-dev @babel/preset-env
-## SCAN FOLDERS
-#for folders in $(find /var/www/projects/fa-nodejs/* -type d )
-#do echo "$folders"
-#done
 ## CLEAN GIT CACHE
 for git_cache in $(git ls-files -i --exclude-standard)
 do echo "$git_cache" && git rm -f --cached "$git_cache"
 done
-## PUBLISH TO NPM
+## PUBLISH TO GIT
 PREV=`git describe --abbrev=0 --tags`
 TAG=(${PREV//./ })
 TAG1=${TAG[0]}
@@ -29,18 +11,20 @@ TAG2=${TAG[1]}
 TAG3=${TAG[2]}
 TAG3=$((TAG3+1))
 NEXT="$TAG1.$TAG2.$TAG3"
+BRANCH=`git branch | grep \* | cut -d ' ' -f2`
 COMMIT=`git rev-parse HEAD`
 MESSAGE=`git log -1 --oneline` #MESSAGE=`git log -1`
 PUBLISHED=`git describe --contains ${COMMIT}`
-if [[ -z "$PUBLISHED" ]]; then
+if [[ "$BRANCH" != "master" ]]; then
+    echo "branch <$BRANCH> cannot be published"
+elif [[ -z "$PUBLISHED" ]]; then
     echo "updating to tag <$NEXT>"
-    npm version ${NEXT} #npm version patch
     git add -A
     git commit -a -m "update to tag <$NEXT> $COMMIT: $MESSAGE"
     git push
-    git tag ${NEXT}
+    npm version ${NEXT} #npm version patch
+#    git tag ${NEXT}
     git push --tags
-    npm publish
 else
     echo "already have tag <$PREV> on commit $COMMIT: $MESSAGE"
 fi
