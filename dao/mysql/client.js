83d8679eb2527816153cbf72acd9412a8045b03e
ivan <ivan@foxart.org>
2019-08-22 17:40:27 +0300

    mysql

---
+++ dao/mysql/client.js
@@ -0,0 +1,134 @@
+"use strict";
+/*nodejs*/
+/** @type {Object} */
+const OracleDb = require("mysql");
+/*fa*/
+const FaError = require("fa-nodejs/base/error");
+const FaDaoClient = require("fa-nodejs/dao/client");
+const FaDaoConnection = require("fa-nodejs/dao/connection");
+
+/*variables*/
+class FaDaoOracleClient extends FaDaoClient {
+	/**
+	 * @constructor
+	 * @param FaDaoOracleModel {FaDaoOracleModel}
+	 */
+	constructor(FaDaoOracleModel) {
+		super();
+		this._FaDaoOracleModel = FaDaoOracleModel;
+	}
+
+	/**
+	 *
+	 * @return {FaDaoOracleConnection}
+	 * @private
+	 */
+	get _connection() {
+		return FaDaoConnection.findConnection(this._FaDaoOracleModel.connection);
+	}
+
+	/**
+	 *
+	 * @return {Promise<Object>}
+	 * @private
+	 */
+	async _connect() {
+
+
+		let options = this._connection.options;
+		Object.entries(options).forEach(function ([key, value]) {
+			OracleDb[key] = value;
+		});
+		OracleDb.queueTimeout = this._connection.timeout;
+		// console.log(this._connection.timeout);
+		return await OracleDb.getConnection({
+			connectString: this._connection.url,
+			user: this._connection.user,
+			password: this._connection.password,
+		});
+	}
+
+	/**
+	 *
+	 * @param error {Error}
+	 * @return {FaError}
+	 * @private
+	 */
+	_error(error) {
+		let MatchPattern = "^(.+): (.+)$";
+		let MatchExpression = new RegExp(MatchPattern);
+		let result = error.message.match(MatchExpression);
+		if (result) {
+			return new FaError({name: result[1], message: result[2]});
+		} else {
+			return new FaError(error);
+		}
+	}
+
+	/**
+	 *
+	 * @return {Promise<Object>}
+	 */
+	async open() {
+		try {
+			let result;
+			if (this._connection.persistent) {
+				if (FaDaoClient.existClient(this._FaDaoOracleModel.connection)) {
+					result = FaDaoClient.findClient(this._FaDaoOracleModel.connection);
+				} else {
+					result = await this._connect();
+					FaDaoClient.attachClient(this._FaDaoOracleModel.connection, result);
+				}
+			} else {
+				// console.info("OPEN", this._connection.timeout);
+				result = await this._connect();
+			}
+			return result;
+		} catch (e) {
+			throw this._error(e);
+		}
+	}
+
+	/**
+	 * @param connection {Object}
+	 * @param query {string}
+	 * @return {Promise<*>}
+	 */
+	async execute(connection, query) {
+		try {
+			return await connection.execute(query);
+		} catch (e) {
+			await this.close(connection);
+			throw this._error(e);
+		}
+	}
+
+	/**
+	 * @param connection {Object}
+	 * @return {Promise<boolean>}
+	 */
+	async close(connection) {
+		try {
+			if (!this._connection.persistent && connection) {
+				// if (!this._connection.persistent) {
+				// 	await connection.release();
+				await connection.close();
+				// console.warn("CLOSE", this._connection.timeout);
+				FaDaoConnection.detachConnection(this._FaDaoOracleModel.connection);
+				FaDaoClient.detachClient(this._FaDaoOracleModel.connection);
+				return true;
+			} else {
+				return false;
+			}
+		} catch (e) {
+			throw this._error(e);
+		}
+	}
+}
+
+/**
+ *
+ * @type {FaDaoOracleClient}
+ */
+module.exports = FaDaoOracleClient;
+
---
+++ dao/mysql/model.js
@@ -0,0 +1,103 @@
+"use strict";
+/*fa*/
+const FaDaoModel = require("fa-nodejs/dao/model");
+const FaDaoMysqlClient = require("fa-nodejs/dao/mysql/client");
+const FaDaoQuery = require("fa-nodejs/dao/query");
+const FaTrace = require("fa-nodejs/base/trace");
+const FaError = require("fa-nodejs/base/error");
+
+class FaDaoOracleModel extends FaDaoModel {
+	/**
+	 * @constructor
+	 */
+	constructor() {
+		super();
+		this._trace = FaTrace.trace(1);
+	};
+
+	get connection() {
+		throw new FaError("connection not specified").setTrace(this._trace);
+	}
+
+	get table() {
+		throw new FaError("table not specified").setTrace(this._trace);
+	}
+
+	/**
+	 *
+	 * @return {FaDaoMysqlClient}
+	 */
+	get client() {
+		if (!this._Client) {
+			this._Client = new FaDaoMysqlClient(this);
+		}
+		return this._Client;
+	}
+
+	/**
+	 *
+	 * @return {FaDaoQuery}
+	 */
+	get query() {
+		if (!this._Query) {
+			this._Query = new FaDaoQuery(this);
+		}
+		return this._Query;
+	}
+
+	/**
+	 *
+	 * @param query
+	 * @return {Promise<Object>}
+	 */
+	async findOne(query) {
+		// console.info(query);
+		let trace = FaTrace.trace(1);
+		let connection = await this.client.open();
+		try {
+			let result = await this.client.execute(connection, query);
+			await this.client.close(connection);
+			// console.error(query, result["rows"]);
+			if (result && result["rows"] && result["rows"][0]) {
+				return result["rows"][0];
+			} else {
+				return null;
+			}
+		} catch (e) {
+			await this.client.close(connection);
+			console.error(query);
+			throw new FaError(e).setTrace(trace);
+		}
+	}
+
+	/**
+	 *
+	 * @param query
+	 * @return {Promise<Array>}
+	 */
+	async findMany(query) {
+		// console.info(query);
+		let trace = FaTrace.trace(1);
+		let connection = await this.client.open();
+		try {
+			let result = await this.client.execute(connection, query);
+			await this.client.close(connection);
+			// let result = await connection.execute(query);
+			if (result && result["rows"]) {
+				return result["rows"]
+			} else {
+				return [];
+			}
+		} catch (e) {
+			await this.client.close(connection);
+			console.error(query);
+			throw new FaError(e).setTrace(trace);
+		}
+	}
+}
+
+/**
+ *
+ * @type {FaDaoOracleModel}
+ */
+module.exports = FaDaoOracleModel;
